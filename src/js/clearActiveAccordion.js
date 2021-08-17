8f23003438f47ed1372229649014f40c3611afb5
Ivan B <ivanbolderoff@gmail.com>
2021-08-17 14:13:18 +0930

    added function descriptions  to clearActiveAccordion and handleClick in src/js and deleted commented out code and unneeded console  logs

--- src/js/clearActiveAccordion.js
+++ src/js/clearActiveAccordion.js
@@ -1,6 +1,13 @@
 const clearActiveAccordion = (allItems) => {
+	/*
+        The purpose of this function is to remove all active accordion-items 
+        so that when a user clicks on a different accordion item, only that 
+        description will appear 
+
+        It takes a nodeList of DOM elements(allItems) which are modified by 
+        this function
+    */
 	allItems.forEach((element) => {
-		// console.log("FIRSST", element.childNodes[1]);
 		element.childNodes[1].classList.remove("bold-header");
 		element.childNodes[3].classList.remove("show");
 	});
--- src/js/handleClick.js
+++ src/js/handleClick.js
@@ -2,12 +2,13 @@ const handleClick = (image, event) => {
 	/*
 		The purpose of this function is to change the image and styling of a 
 		accordion item once the user clicks on it. 
-		This is done by removing and adding the class .show and .bold-header depending
-		on which event is clicked.
+		This is done and adding the class .show and depending
+		on which event is clicked as well as changing the 
+        image path and alt-text
 
-		Ideally this function would be declared and implemented in another 
-		file then imported but I am relying on the golobally defined var
-		allEvents to remove the styling onClick
+        This function takes a DOM element containing an image, and a DOM element 
+        that is a member of an accordion
+
 	*/
 
 	console.log("The event clicked:", event.path);
@@ -23,25 +24,26 @@ const handleClick = (image, event) => {
 	relevantChild.classList.add("show");
 
 	let header;
+
+	//turney operator used so if user clicks on the title of the event
+	// no error will occur
 	event.path[0].classList.contains("accordion-header")
 		? (header = event.path[0])
 		: (header = event.path[0].childNodes[1]);
-	// const header = event.path[0].childNodes[1];
-	console.log(image);
+
+	//to bold the header when active
+	header.classList.add("bold-header");
+
 	// change the image depending on the event
 	if (header.id === "heading-Sydney") {
 		image.src = "./images/vivid-sydney.jpg";
 		image.alt = "Sydney opera house at night";
-		//to bold the header when active
-		header.classList.add("bold-header");
 	} else if (header.id === "heading-Queens-bday") {
 		image.src = "./images/queens-birthday-celebrations.jpg";
 		image.alt = "Close-up of sparklers";
-		header.classList.add("bold-header");
 	} else if (header.id === "heading-Fathers-day") {
 		image.src = "./images/fathers-day-dining.jpg";
 		image.alt = "Close-up of clam chowder";
-		header.classList.add("bold-header");
 	}
 };
 
