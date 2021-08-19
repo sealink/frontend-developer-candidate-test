a327360b997d8d8ac38abfef78c7b393e409c78a
Jinjoo Moon <jinjoo.pearl.moon@gmail.com>
2021-08-19 12:23:21 +0930

    What's On component (Jinjoo Moon)
    Test for the frontend developer position

---
+++ app/dist/app.js
@@ -0,0 +1,119 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/scss/app.scss":
+/*!***************************!*\
+  !*** ./src/scss/app.scss ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://sealink-frontend-dev-test/./src/scss/app.scss?");
+
+/***/ }),
+
+/***/ "./src/entry.js":
+/*!**********************!*\
+  !*** ./src/entry.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_app_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./scss/app.scss */ \"./src/scss/app.scss\");\n/* harmony import */ var _js_app_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./js/app.js */ \"./src/js/app.js\");\n/* harmony import */ var _js_app_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_js_app_js__WEBPACK_IMPORTED_MODULE_1__);\n\n\n\n\n//# sourceURL=webpack://sealink-frontend-dev-test/./src/entry.js?");
+
+/***/ }),
+
+/***/ "./src/js/app.js":
+/*!***********************!*\
+  !*** ./src/js/app.js ***!
+  \***********************/
+/***/ (() => {
+
+eval("document.addEventListener('DOMContentLoaded', () => {\n  const headings = document.querySelectorAll('.card-header');\n  const headingTexts = document.querySelectorAll('.headingText');\n  const whatsonImg = document.getElementById(\"whatsonImg\");\n  headingTexts[0].style.fontWeight = \"700\";\n  for (let i = 0; i < headings.length; i++) {\n    const currentHeading = headings[i];\n    currentHeading.addEventListener('click', function() {\n      // makes all headingTexts fontWeight as default (400)\n      for (let i = 0; i < headingTexts.length; i++) {\n        headingTexts[i].style.fontWeight = \"400\";\n      }\n      // changes Img to relevant image & alt\n      whatsonImg.src = currentHeading.getAttribute(\"data-img-src\"); \n      whatsonImg.alt = currentHeading.getAttribute(\"data-img-alt\");\n      // makes clicked headerText fontWeight as 700\n      const currentHeadingText = currentHeading.getElementsByTagName(\"h4\");\n      currentHeadingText[0].style.fontWeight = \"700\";\n      // open when it is opened\n      const currentHeadingStatus = currentHeading.getElementsByTagName(\"button\");\n      const currentSibling = currentHeading.nextElementSibling;\n      if (currentHeadingStatus[0].getAttribute(\"aria-expanded\") == \"true\") {\n        currentHeadingStatus[0].setAttribute(\"aria-expanded\", \"true\");\n        currentSibling.classList.add(\"show\");\n      } \n    });\n  }\n});\n\n\n\n//# sourceURL=webpack://sealink-frontend-dev-test/./src/js/app.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/entry.js");
+/******/ 	
+/******/ })()
+;
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,29 @@
-// Add your custom JavaScript here
+document.addEventListener('DOMContentLoaded', () => {
+  const headings = document.querySelectorAll('.card-header');
+  const headingTexts = document.querySelectorAll('.headingText');
+  const whatsonImg = document.getElementById("whatsonImg");
+  headingTexts[0].style.fontWeight = "700";
+  for (let i = 0; i < headings.length; i++) {
+    const currentHeading = headings[i];
+    currentHeading.addEventListener('click', function() {
+      // makes all headingTexts fontWeight as default (400)
+      for (let i = 0; i < headingTexts.length; i++) {
+        headingTexts[i].style.fontWeight = "400";
+      }
+      // changes Img to relevant image & alt
+      whatsonImg.src = currentHeading.getAttribute("data-img-src"); 
+      whatsonImg.alt = currentHeading.getAttribute("data-img-alt");
+      // makes clicked headerText fontWeight as 700
+      const currentHeadingText = currentHeading.getElementsByTagName("h4");
+      currentHeadingText[0].style.fontWeight = "700";
+      // open when it is opened
+      const currentHeadingStatus = currentHeading.getElementsByTagName("button");
+      const currentSibling = currentHeading.nextElementSibling;
+      if (currentHeadingStatus[0].getAttribute("aria-expanded") == "true") {
+        currentHeadingStatus[0].setAttribute("aria-expanded", "true");
+        currentSibling.classList.add("show");
+      } 
+    });
+  }
+});
+
