7f653a61f5df2ffa96d4224b6dcc33248cf2fdd8
Yuhen LI <yuhenl@student.unimelb.edu.au>
2021-08-25 08:07:56 +1000

    YuhenLi's frontend accordion assignment

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,18 @@
 // Add your custom JavaScript here
+window.onload = function add(){
+    var accItem = document.getElementsByClassName('card');
+    var accHD = document.getElementsByClassName('heading');
+    for (i = 0; i < accHD.length; i++) {
+        accHD[i].addEventListener('click', toggleItem, false);
+    }
+    function toggleItem() {
+        var itemClass = this.parentNode.className;
+        //console.log("this is card:" + itemClass)
+        for (i = 0; i < accItem.length; i++) {
+            accItem[i].className = 'card close';
+        }
+        if (itemClass == 'card close') {
+            this.parentNode.className = 'card show';
+        }
+    }
+    }
